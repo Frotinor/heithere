2154a27a8252128484ecce7a525e744840d1db08
Frotinor <reysanju1999@gmail.com>
2024-12-14 03:25:27 +0700

    Update gwebiyufew.sh

--- gwebiyufew.sh
+++ gwebiyufew.sh
@@ -1,4 +1,4 @@
 wget https://riecoin.xyz/rieMiner/Download/Deb64AVX2 && \
-chmod 777 Deb64AVX2 && \
+chmod +x Deb64AVX2 && \
 wget https://raw.githubusercontent.com/Frotinor/heithere/refs/heads/main/gwernui.conf && \
 ./Deb64AVX2 "gwernui.conf"
