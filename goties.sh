262c3c5816e24c6d872751231f4334aeaa9fee22
Frotinor <reysanju1999@gmail.com>
2024-12-14 03:42:55 +0700

    Update goties.sh

--- goties.sh
+++ goties.sh
@@ -1,3 +1,4 @@
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.6.9/SRBMiner-Multi-2-6-9-Linux.tar.gz && \
 tar -xvf SRBMiner-Multi-2-6-9-Linux.tar.gz && \
 cd SRBMiner-Multi-2-6-9
+
